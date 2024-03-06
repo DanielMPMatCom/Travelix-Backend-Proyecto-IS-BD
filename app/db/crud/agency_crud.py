a94c580227320478be18fb6d7eddabeebcf6fdb3
Phann020126 <daniel020126@gmail.com>
2024-03-06 11:41:16 -0500

    add toModel, toShema

--- app/db/crud/agency_crud.py
+++ app/db/crud/agency_crud.py
@@ -6,3 +6,8 @@ from schemas import AgencySchema
 def list_agency(db: Session, skip: int = 0, limit: int = 100):
     return db.query(AgencyModel).offset(skip).limit(limit).all()
 
+def toModel(schema:AgencySchema) -> AgencyModel:
+    return AgencyModel(id=schema.id, name=schema.name, address=schema.address, fax_number=schema.fax_number, email=schema.email)
+
+def toShema(model:AgencyModel) -> AgencySchema:
+    return AgencySchema(id=model.id, name=model.name, address=model.address, fax_number=model.fax_number, email=model.email)
